231876df701008dab73dc9cbb7564ecdcffc093e
nmangano <nmangano@NB-10.applus>
2023-03-07 17:36:09 -0300

    Fix errores en bknd netbeans

--- src/main/java/com/portfolio/portfolio_ArgentinaPrograma/Controlador/PersonaControlador.java
+++ src/main/java/com/portfolio/portfolio_ArgentinaPrograma/Controlador/PersonaControlador.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,18 @@ public class PersonaControlador {
         List<Persona> list = personaServicio.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
+    
+    @PostMapping("/crear")
+    public String createPersona(@RequestBody Persona persona){
+    personaServicio.save(persona);
+    return "La Persona fue creada OK";
+    }
+    @DeleteMapping("/delete/{id}")
+    public String deletePersona(@PathVariable int id){
+    personaServicio.delete(id);
+    return "La persona fue eliminada OK";
+    }
+    
     
     @GetMapping("/detail/{id}")
     public ResponseEntity<Persona> getById(@PathVariable("id") int id){
